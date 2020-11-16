4a69b83e398877bc26b6a0db39489e2160be899d
CLRminded <the_rocket_scientist@icloud.com>
2020-11-15 21:21:00 -0800

    Initial Commit

---
+++ programmatic1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  programmatic1
+//
+//  Created by Christopher Liudahl-Rackley on 11/15/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ programmatic1/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  programmatic1
+//
+//  Created by Christopher Liudahl-Rackley on 11/15/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
